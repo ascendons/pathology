23a4d367e72e66f98c0615da724c4f6d42444c25
Sid <aishwaryakumari@Aishwaryas-MacBook-Pro.local>
2025-05-04 23:57:03 +0530

    fixed build err

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from './login/environment';
+import { environment } from '../environment';
 import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
